d74856a865e085fc7011603b4f4a2f3e1a09d74f
Aayush Badoni <aayushbadoni2005@gmail.com>
2026-07-12 12:27:08 +0530

    Time: 24 ms (44.97%), Space: 102.9 MB (63.79%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        stack<pair<int,int>>st;
+        vector<int>res(n,0);
+        for(int i = n-1; i>=0; i--){
+            while(!st.empty() && st.top().first<=temperatures[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                res[i] = 0;
+            }
+            else{
+                res[i] = st.top().second-i;
+            }
+            st.push({temperatures[i],i});
+        }
+        return res;
+    }
+};
