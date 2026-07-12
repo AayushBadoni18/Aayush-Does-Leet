c542fcd088ef107a3f8cad53742a1aeb5ef021cd
Aayush Badoni <aayushbadoni2005@gmail.com>
2026-07-12 15:33:54 +0530

    Time: 0 ms (100%), Space: 17.6 MB (39.31%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
-    bool check(int node, int parent, vector<int>& vis,
-               vector<vector<int>>& graph, int color) {
-
-        queue<pair<pair<int,int>,int>> q;
-
-        vis[node] = color;
-        q.push({{node, parent}, color});
-
-        while(!q.empty()) {
-
-            int node = q.front().first.first;
-            int parent = q.front().first.second;
-            int color = q.front().second;
-            q.pop();
-
-            for(auto it : graph[node]) {
-
-                if(vis[it] == -1) {
-                    vis[it] = !color;
-                    q.push({{it, node}, !color});
-                }
-                else if(vis[it] == color) {
-                    return false;
-                }
-            }
+    bool dfs(vector<int>& color, vector<int>& vis, bool b, vector<vector<int>>& graph, int u) {
+        if(color[u]==!b) return false;
+        color[u] = b;
+        if(vis[u]) return true;
+        vis[u] = 1;
+        for(int v : graph[u]) {
+            if(!dfs(color, vis, !b, graph, v)) return false;
         }
-
         return true;
     }
-
     bool isBipartite(vector<vector<int>>& graph) {
-
-        vector<int> vis(graph.size(), -1);
-
-        for(int i = 0; i < graph.size(); i++) {
-            if(vis[i] == -1) {
-                if(!check(i, -1, vis, graph, 0))
-                    return false;
-            }
+        vector<int> color(graph.size(), -1);
+        vector<int> vis(graph.size(), 0);
+        for(int i = 0; i<graph.size(); i++) {
+            if(!vis[i] && !dfs(color, vis, 0, graph, i)) return false;
         }
-
         return true;
     }
 };
